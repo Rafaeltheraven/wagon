3f57cf47ab048e4c0a7527a6239d73e2c6e305ac
Rafaeltheraven <rafael@dulfer.be>
2023-06-13 11:25:57 +0200

    properly lex/parse idents

--- wagon-macros/src/lib.rs
+++ wagon-macros/src/lib.rs
@@ -1,15 +1,19 @@
-use proc_macro::TokenStream;
 use extendable_enums::extendable_enum;
-use quote::quote;
 
 #[extendable_enum(inherit_from_base)]
 #[derive(Logos, Debug, PartialEq)]
 #[logos(skip r"[ \t\n\f]+")]
 enum Base {
-    #[regex("([a-zA-Z][a-zA-Z0-9]*)", |lex| lex.slice().to_owned())]
-    Ident(String),
+    #[regex("(\\$|!)?([a-zA-Z][a-zA-Z0-9]*)", |lex| detect_ident_type(lex.slice()))]
+    Identifier(Ident),
 
     #[regex("\"([^\"\\\\]|\\\\.)*\"", |lex| rem_first_and_last_char(lex.slice()))]
     #[regex("\'([^\'\\\\]|\\\\.)*\'", |lex| rem_first_and_last_char(lex.slice()))]
     LitString(String),
+
+    #[token("[")]
+    LBr,
+
+    #[token("]")]
+    RBr,
 }
