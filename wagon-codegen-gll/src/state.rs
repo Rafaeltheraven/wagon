d307847cc4a6e904b411b800fc662bab852ebba7
Rafaeltheraven <rafael@dulfer.be>
2024-02-12 12:47:04 +0100

    more properly output result in gll binary

--- wagon-codegen-gll/src/state.rs
+++ wagon-codegen-gll/src/state.rs
@@ -416,7 +416,10 @@ impl CodeGenState {
     			#stream
     			let mut state = wagon_gll::GLLState::init(contents, label_map, rule_map, regex_map).unwrap();
     			state.main();
-    			println!("{}", state.print_sppf_dot(crop).unwrap());
+    			match state.print_sppf_dot(crop) {
+			        Ok(t) => println!("{t}"),
+			        Err(e) => println!("Error: {e}")
+			    }
     			assert!(state.accepts());
     		}
     	))
