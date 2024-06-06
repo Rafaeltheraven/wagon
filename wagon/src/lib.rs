8fb2bf7e175decf2406d55ba96fa61d37d8d50d6
Rafaeltheraven <rafael@dulfer.be>
2024-06-06 16:04:39 +0200

    create wagon meta-package

---
+++ wagon/src/lib.rs
@@ -0,0 +1,16 @@
+//! WAGon is a Library/Ecosystem/Proof-of-Concept for working with Weighted Attribute Grammars.
+//! WAGon is designed for researchers and language designers experimenting with WAGs such that they do not have to waste time creating a DSL and parser
+//! and can start immediately experimenting with the format itself.
+//!
+//! You can read the [paper](./paper.pdf) which explains more in detail what WAGs are, what WAGon is and why you should care.
+//!
+//! WAGon consists out of the following crates:
+//!
+//! * [`wagon-parser`](../wagon_parser/)
+//! * [`wagon-codegen`](../wagon_codegen/)
+//! * [`wagon-lexer`](../wagon_lexer/)
+//! * [`wagon-value`](../wagon_value/)
+//! * [`wagon-utils`](../wagon_utils/)
+//! * [`wagon-macros`](../wagon_macros/)
+//! * [`wagon-ident`](../wagon_ident/)
+//! 
